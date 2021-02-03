fb9cd4c6e6a2061bb3d1b0f01c6058670cd8f8a8
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-03 23:41:26 +0000

    4-puts.c script fixed 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/*
+/**
  *main: Entry point.
  *void: Indicates that the main() function will not return any value.
  *function: This function prints the string "Programming is like building a multilingual puzzle"
