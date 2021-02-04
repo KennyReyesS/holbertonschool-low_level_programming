7db21fe44bb41d50c44fb3a047f1cf08253f149a
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-04 23:19:59 +0000

    6-print_numberz.c program added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *main - Prints numbers 0-9 and not allowed to use any variable of type char.
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
