a88f88e68ec867018c37f1570f3f189b812debf2
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-04 00:00:11 +0000

    4-puts.c script fixed 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 /**
- *main - Entry point.
- *void - Indicates that the main() function will not return any value.
- *function - Prints the string "Programming is like" with puts.
- *Return: 0
+ * main - Entry point.
+ *
+ * function - Prints the string "Programming is like" with puts.
+ *
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
