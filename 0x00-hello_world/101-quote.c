08c7e5b3301f89cad7031f055a6aab73f21e5fe7
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-04 02:17:42 +0000

    101-quote.c script added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - prints "and that piece of art is useful"
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
