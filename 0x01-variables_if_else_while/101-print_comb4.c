629ca9709cd82b86318731a3b8caf92b64c2fc75
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-06 04:09:24 +0000

    101-print_comb4.c program added

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i == 55 && j == 56 && k == 57)
+				{
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
