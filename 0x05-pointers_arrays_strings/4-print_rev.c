23f11e09740a2bb9a596a94ada926bd884b2a67f
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-17 03:43:23 +0000

    4-print_rev.c function added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_rev - prints a string, in reverse, followed by a new line.
+ *@s: char
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	while (count >= 0)
+	{
+		_putchar(s[count--]);
+	}
+	_putchar('\n');
+}
