02c5ab6db6bef38475260050250bc284fc359646
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-17 19:15:52 +0000

    9-strcpy.c function fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ *_strcpy - Copies the string pointed to by src, including the (\0), to dest.
+ *@dest: char
+ *@src: char
+ *Return: 0
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int count;
