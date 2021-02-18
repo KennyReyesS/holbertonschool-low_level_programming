5fa5bcf8f146d2a1341c0406fd1f95982e9a45b3
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-18 01:47:53 +0000

    7-puts_half.c fixed 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,32 +1,21 @@
 #include "holberton.h"
 /**
  *puts_half - prints half of a string, followed by a new line.
- *@str: 0123456789'\0'
+ *@str: char string.
  *Return: 0
  */
 void puts_half(char *str)
 {
 	int n;
-	int length;
 
 	n = 0;
 	while (str[n] != '\0')
 	{
 		n++;
 	}
-	if (n % 2 == 1)
+	for (n = n / 2; str[n] != '\0'; n++)
 	{
-		length = (n - 1) / 2;
-		length += 1;
-	}
-	else
-	{
-		length = n / 2;
-	}
-
-	for (; length < n; length++)
-	{
-		_putchar(str[length]);
+		_putchar(str[n]);
 	}
 	_putchar('\n');
 }
