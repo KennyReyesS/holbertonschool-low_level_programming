52282460fc5d1d9eb8247e40c4407eae2fe12846
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-19 06:22:19 +0000

    3-strcmp.c function created 2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	if (s1[i] == s2[i])
+	while (s1[i] == s2[i])
 	{
 		return (0);
+		i++;
 	}
-	else
-	{
-		return (s1[i] - s2[i]);
-	}
+	return (s1[i] - s2[i]);
 }
