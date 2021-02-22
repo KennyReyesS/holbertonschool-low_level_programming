886190c52377fa36aa53e34a024db50fba95d76e
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-22 21:49:35 +0000

    2-strchr.c function fixed 5

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
 		if (s[i] == c)
+		{
 			return (s + i);
+		}
+		i++;
 	}
-	return (0);
+	return ('\0');
 }
