8b2305b14191778c13570a88056ac9c6b57b4ecd
KennyReyesS <kennyreyes2000@gmail.com>
2021-02-23 00:11:05 +0000

    4-strpbrk.c function created1

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ *_strpbrk - function that searches a string for any of a set of bytes.
+ *@s: char string 1
+ *@accept: char string 2
+ *Return: 0 or (s + i) if s[i] == accept[j]
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
