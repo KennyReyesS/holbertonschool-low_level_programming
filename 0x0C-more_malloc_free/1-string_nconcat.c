702640b057e35e41db0eb11195d3bd6191188acf
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-04 22:03:39 +0000

    1-string_nconcat.c function created1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = sl2;
 	}
 
-	str = malloc(sizeof(char) * sl1 + sl2 + 1);
+	str = malloc(sizeof(char) * sl1 + n + 1);
 	if (str == NULL)
 	{
 		return (NULL);
