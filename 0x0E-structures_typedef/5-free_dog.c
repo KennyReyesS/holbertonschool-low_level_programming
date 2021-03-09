b2da77fdf576548b6a1f2ed5a05319ca3a97256c
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-09 22:24:12 +0000

    5-free_dog.c function created

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *free_dog - frees dogs.
+ *@d: dog data
+ *Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
