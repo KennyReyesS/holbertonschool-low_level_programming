6373ec38b4248c1624fbc941bdeceed0828703e6
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-12 14:56:52 +0000

    2-int_index.c created

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,19 +12,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL && cmp == NULL)
+	if (size > 0)
 	{
-		return (-1);
-	}
-	else
-	{
-		for (i = 0; i < size; i++)
+		if (array != NULL && cmp != NULL)
 		{
-			if ((*cmp)(array[i]) != 0)
+			for (i = 0; i < size; i++)
 			{
-				return (i);
+				if ((*cmp)(array[i]) != 0)
+				{
+					return (i);
+				}
 			}
 		}
 	}
-	return (i);
+	return (-1);
 }
