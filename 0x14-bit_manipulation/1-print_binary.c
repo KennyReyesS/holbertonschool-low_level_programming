e85904d1f7d016b9a89a3bfe1a2eafb2e3bc2ba3
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-26 14:36:13 +0000

    function created

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,31 @@
 #include "holberton.h"
 
 /**
+ *divideNumber - divides the number and obtain the remainder.
+ *@n: number in decimal.
+ */
+
+void divideNumber(unsigned long int n)
+{
+	if (n < 1)
+	{
+		return;
+	}
+	divideNumber(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
+/**
  *print_binary - prints the binary representation of a number.
- *@n: number in decimal
+ *@n: number in decimal.
  */
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n == 0)
 	{
-		print_binary(n >> 1);
+		_putchar('0');
+		return;
 	}
-	_putchar((n & 1) + '0');
+	divideNumber(n);
 }
