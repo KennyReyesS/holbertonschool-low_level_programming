424f8dc7433428f310e2d0abf7a1cc2a7e64d6fd
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-26 00:25:20 +0000

    condition for b null added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,8 +4,8 @@
  *binary_to_uint - converts a binary number to an unsigned int.
  *@b: pointer to a string of 0 and 1 chars.
  *Return: the converted number, or 0 if:
- *-there is one or more chars in the string b that is not 0 or 1.
- *-b is NULL.
+ *1. there is one or more chars in the string b that is not 0 or 1.
+ *2. b is NULL.
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -14,30 +14,37 @@ unsigned int binary_to_uint(const char *b)
 	int lastIndex;
 	int i = 0, j = 0, bit = 0;
 
-	while (b[i] != '\0')
+	if (b != NULL)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		while (b[i] != '\0')
 		{
-			return (0);
+			if (b[i] != '0' && b[i] != '1')
+			{
+				return (0);
+			}
+			i++;
 		}
-		i++;
+		lastIndex = i - 1;
+		
+		while (lastIndex >= 0)
+		{
+			if (b[lastIndex] == '0')
+			{
+				bit = 0;
+			}
+			else
+			{
+				bit = 1;
+			}
+			result += bit << j;
+			
+			lastIndex--;
+			j++;
+		}
 	}
-	lastIndex = i - 1;
-
-	while (lastIndex >= 0)
+	else
 	{
-		if (b[lastIndex] == '0')
-		{
-			bit = 0;
-		}
-		else
-		{
-			bit = 1;
-		}
-		result += bit << j;
-
-		lastIndex--;
-		j++;
+		return (0);
 	}
 	return (result);
 }
