497507c612260406c76432696cb58da9c8ee2881
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-26 01:12:37 +0000

    commits added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,18 +1,32 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ *_strlen - prints the length of a string.
+ *@s: pointer to the string
+ *Return: the length
+ */
+
 int _strlen(char *s)
 {
 	int i;
 
 	i = 0;
-	while(*(s + i))
+	while (*(s + i))
 	{
 		i++;
 	}
 	i++;
 	return (i);
 }
+
+/**
+ *_strcpy - copy a string
+ *@dest: pointer to the string for copying
+ *@src: pointer to the string src
+ *Return: the string copied
+ */
 
 char *_strcpy(char *dest, char *src)
 {
@@ -26,6 +40,14 @@ char *_strcpy(char *dest, char *src)
 	*(dest + count) = '\0';
 	return (dest);
 }
+
+/**
+ *new_dog - function that creates a new dog.
+ *@name: pointer to the name in the struct.
+ *@age: age of the dog.
+ *@owner: pointer to the owner in the struct.
+ *Return: the string of the struct
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
