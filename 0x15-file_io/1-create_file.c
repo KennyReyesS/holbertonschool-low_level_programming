54175b36ef9fd3a0ea7805067b4caaac1374877d
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-31 15:42:59 +0000

    condition added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,9 +22,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	while (text_content[i] != '\0')
+	if (text_content != NULL)
 	{
-		i++;
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
 	}
 	wr_str = write(fd, text_content, i);
 
