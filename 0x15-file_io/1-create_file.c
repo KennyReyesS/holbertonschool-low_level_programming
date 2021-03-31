7418fc7ffea98d6ef5fc82f6d41cfb300c229e29
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-31 04:50:16 +0000

    condition for filename added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,6 +30,10 @@ int create_file(const char *filename, char *text_content)
 	int fd, nr_bytes;
 	int len = _strlen(text_content);
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
