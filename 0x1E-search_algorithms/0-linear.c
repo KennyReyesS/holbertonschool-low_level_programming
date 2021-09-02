cbe905212b2ab4b70bdd2e048e46634fe5c003e0
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-02 02:30:19 +0000

    indent fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,20 +11,19 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int i;
+	unsigned int i;
 
-    if (array == NULL)
-    {
-	return (-1);
-    }
-
-    for (i = 0; i < size; i++)
-    {
-	printf("Value checked array[%d] = [%d]\n", i, array[i]);
-	if (array[i] == value)
+	if (array == NULL)
 	{
-	    return (i);
+		return (-1);
 	}
-    }
-    return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
